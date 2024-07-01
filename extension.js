4b0ada5a50ea8e67bf94033f8663a2718a33cadf
Jason Balaci <jason@balaci.me>
2024-07-01 16:32:13 -0400

    Rename app class to project name

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-export default class StealMyFocus extends Extension {
+export default class NoAnnoyance extends Extension {
   constructor(metadata) {
     super(metadata);
   }

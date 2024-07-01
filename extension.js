b901292eb4643972b3025b400b2b300a29556a9b
Jason Balaci <jason@balaci.me>
2024-07-01 19:01:12 -0400

    Rebuild preferences window, fixing saving

--- extension.js
+++ extension.js
@@ -1,5 +1,5 @@
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
 export default class NoAnnoyance extends Extension {
   constructor(metadata) {
@@ -9,24 +9,32 @@ export default class NoAnnoyance extends Extension {
   enable() {
     console.debug("Disabling 'Window Is Ready' Notification");
 
-    this._windowDemandsAttentionId = global.display.connect('window-demands-attention', this._onWindowDemandsAttention.bind(this));
-    this._windowMarkedUrgentId = global.display.connect('window-marked-urgent', this._onWindowDemandsAttention.bind(this));
+    this._windowDemandsAttentionId = global.display.connect(
+      "window-demands-attention",
+      this._onWindowDemandsAttention.bind(this)
+    );
+
+    this._windowMarkedUrgentId = global.display.connect(
+      "window-marked-urgent",
+      this._onWindowDemandsAttention.bind(this)
+    );
   }
 
   disable() {
-    console.debug("Reenabling 'Window Is Ready' Notification");
+    console.debug("Enabling 'Window Is Ready' Notification");
 
     global.display.disconnect(this._windowDemandsAttentionId);
     global.display.disconnect(this._windowMarkedUrgentId);
   }
 
   _onWindowDemandsAttention(display, window) {
-    if (!window || window.has_focus() || window.is_skip_taskbar())
-      return;
+    if (!window || window.has_focus() || window.is_skip_taskbar()) return;
 
+    // TODO: Settings should be cached
     let settings = this.getSettings();
-    let blocklist = settings.get_strv('blocklist');
 
+    // TODO: blocklist should be cached as a Set, and the 'includes' should be replaced with a 'has'
+    let blocklist = settings.get_strv("blocklist");
     if (!blocklist.includes(window.get_wm_class())) {
       Main.activateWindow(window);
     }
